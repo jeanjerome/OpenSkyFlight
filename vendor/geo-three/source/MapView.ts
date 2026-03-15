a2181a2c9fa2d4a5b133bfef96c163e82b10cb21
jeanjerome <jeanjerome.levy@gmail.com>
2026-03-15 14:39:04 +0100

    chore: add geo-three source code

---
+++ vendor/geo-three/source/MapView.ts
@@ -0,0 +1,287 @@
+import {BufferGeometry, Camera, Group, Material, Mesh, MeshBasicMaterial, Object3D, Raycaster, Scene, WebGLRenderer} from 'three';
+import {OpenStreetMapsProvider} from './providers/OpenStreetMapsProvider';
+import {MapNode} from './nodes/MapNode';
+import {MapHeightNode} from './nodes/MapHeightNode';
+import {MapPlaneNode} from './nodes/MapPlaneNode';
+import {MapSphereNode} from './nodes/MapSphereNode';
+import {MapHeightNodeShader} from './nodes/MapHeightNodeShader';
+import {LODRaycast} from './lod/LODRaycast';
+import {MapProvider} from './providers/MapProvider';
+import {LODControl} from './lod/LODControl';
+import {MapMartiniHeightNode} from './nodes/MapMartiniHeightNode';
+
+/**
+ * Map viewer is used to read and display map tiles from a server.
+ *
+ * It was designed to work with a OpenMapTiles but can also be used with another map tiles.
+ *
+ * The map is drawn in plane map nodes using a quad tree that is subdivided as necessary to guaratee good map quality.
+ */
+export class MapView extends Mesh 
+{
+	/**
+	 * Planar map projection.
+	 */
+	public static PLANAR: number = 200;
+
+	/**
+	 * Spherical map projection.
+	 */
+	public static SPHERICAL: number = 201;
+
+	/**
+	 * Planar map projection with height deformation.
+	 */
+	public static HEIGHT: number = 202;
+
+	/**
+	 * Planar map projection with height deformation using the GPU for height generation.
+	 */
+	public static HEIGHT_SHADER: number = 203;
+
+	/**
+	 * RTIN map mode.
+	 */
+	public static MARTINI: number = 204;
+
+	/**
+	 * Map of the map node types available.
+	 */
+	public static mapModes: Map<number, any> = new Map<number, any>([
+		[MapView.PLANAR, MapPlaneNode],
+		[MapView.SPHERICAL, MapSphereNode],
+		[MapView.HEIGHT, MapHeightNode],
+		[MapView.HEIGHT_SHADER, MapHeightNodeShader],
+		[MapView.MARTINI, MapMartiniHeightNode]
+	]);
+
+	/**
+	 * LOD control object used to defined how tiles are loaded in and out of memory.
+	 */
+	public lod: LODControl = null;
+
+	/**
+	 * Map tile color layer provider.
+	 */
+	public provider: MapProvider = null;
+
+	/**
+	 * Map height (terrain elevation) layer provider.
+	 * 
+	 * Only used for HEIGHT, HEIGHT_SHADER and MARTINI map modes.
+	 */
+	public heightProvider: MapProvider = null;
+
+	/**
+	 * Define the type of map node in use, defined how the map is presented.
+	 *
+	 * Should only be set on creation.
+	 */
+	public root: MapNode = null;
+
+	/**
+	 * Indicate if the nodes should cache its children when it is simplified. Nodes that are no longer in use should be kept in memory.
+	 * 
+	 * Usefull for fast moving scenarios to prevent reparsing data in fast moving scenes.
+	 * 
+	 * Should only be used if the child generation process is time consuming. Should be kept off unless required.
+	 */
+	public cacheTiles: boolean = false;
+
+	/**
+	 * Constructor for the map view objects.
+	 *
+	 * @param root - Map view node modes can be SPHERICAL, HEIGHT or PLANAR. PLANAR is used by default. Can also be a custom MapNode instance.
+	 * @param provider - Map color tile provider by default a OSM maps provider is used if none specified.
+	 * @param heightProvider - Map height tile provider, by default no height provider is used.
+	 */
+	public constructor(root: (number | MapNode) = MapView.PLANAR, provider: MapProvider = new OpenStreetMapsProvider(), heightProvider: MapProvider = null, lod: LODControl = new LODRaycast()) 
+	{
+		super(undefined, new MeshBasicMaterial({transparent: true, opacity: 0.0, depthWrite: false, colorWrite: false}));
+
+		this.lod = lod;
+
+		this.provider = provider;
+		this.heightProvider = heightProvider;
+
+		this.setRoot(root);
+		this.preSubdivide();
+	}
+
+	/**
+	 * Ajust node configuration depending on the camera distance.
+	 *
+	 * Called everytime automatically before render by the renderer.
+	 */
+	public onBeforeRender: (renderer: WebGLRenderer, scene: Scene, camera: Camera, geometry: BufferGeometry, material: Material, group: Group)=> void = (renderer, scene, camera, geometry, material, group) => 
+	{
+		this.lod.updateLOD(this, camera, renderer, scene);
+	};
+
+	/**
+	 * Set the root of the map view.
+	 *
+	 * Is set by the constructor by default, can be changed in runtime.
+	 *
+	 * @param root - Map node to be used as root.
+	 */
+	public setRoot(root: (MapNode | number)): void
+	{
+		if (typeof root === 'number') 
+		{
+			if (!MapView.mapModes.has(root)) 
+			{
+				throw new Error('Map mode ' + root + ' does is not registered.');
+			}
+
+			const rootConstructor = MapView.mapModes.get(root);
+
+			// @ts-ignore
+			root = new rootConstructor(null, this);
+		}
+
+		// Remove old root
+		if (this.root !== null) 
+		{
+			this.remove(this.root);
+			this.root = null;
+		}
+
+		// @ts-ignore
+		this.root = root;
+
+		// Initialize root node
+		if (this.root !== null) 
+		{
+			// @ts-ignore
+			this.geometry = this.root.constructor.baseGeometry;
+			// @ts-ignore
+			this.scale.copy(this.root.constructor.baseScale);
+
+			this.root.mapView = this;
+			this.add(this.root);
+			this.root.initialize();
+		}
+	}
+
+	/**
+	 * Pre-subdivide map tree to create nodes of levels not available in the provider.
+	 * 
+	 * Checks for the minimum zoom level in the providers attached to the map view.
+	 */
+	public preSubdivide(): void 
+	{
+		function subdivide(node: MapNode, depth: number): void 
+		{
+			if (depth <= 0) 
+			{
+				return;
+			}
+
+			node.subdivide();
+
+			for (let i = 0; i < node.children.length; i++) 
+			{
+				if (node.children[i] instanceof MapNode) 
+				{
+					const child = node.children[i] as MapNode;
+					subdivide(child, depth - 1);
+				}
+			}
+		}
+
+		const minZoom = Math.max(this.provider.minZoom, this.heightProvider?.minZoom ?? -Infinity);
+		if (minZoom > 0) 
+		{
+			subdivide(this.root, minZoom);
+		}
+	}
+
+	/**
+	 * Change the map provider of this map view.
+	 *
+	 * Will discard all the tiles already loaded using the old provider.
+	 */
+	public setProvider(provider: MapProvider): void
+	{
+		if (provider !== this.provider) 
+		{
+			this.provider = provider;
+			this.clear();
+		}
+	}
+
+	/**
+	 * Change the map height provider of this map view.
+	 *
+	 * Will discard all the tiles already loaded using the old provider.
+	 */
+	public setHeightProvider(heightProvider: MapProvider): void
+	{
+		if (heightProvider !== this.heightProvider) 
+		{
+			this.heightProvider = heightProvider;
+			this.clear();
+		}
+	}
+
+	/**
+	 * Clears all tiles from memory and reloads data. Used when changing the provider.
+	 *
+	 * Should be called manually if any changed to the provider are made without setting the provider.
+	 */
+	public clear(): any
+	{
+		this.traverse(function(children: Object3D): void
+		{
+			// @ts-ignore
+			if (children.childrenCache) 
+			{
+				// @ts-ignore
+				children.childrenCache = null;
+			}
+
+			// @ts-ignore
+			if (children.initialize) 
+			{
+				// @ts-ignore
+				children.initialize();
+			}
+		});
+
+		return this;
+	}
+
+	/**
+	 * Get the minimum zoom level available in the providers attached to the map view.
+	 * 
+	 * @returns Minimum zoom level available.
+	 */
+	public minZoom(): number 
+	{
+		return Math.max(this.provider.minZoom, this.heightProvider?.minZoom ?? -Infinity);
+	}
+
+	/**
+	 * Get the maximum zoom level available in the providers attached to the map view.
+	 * 
+	 * @returns Maximum zoom level available.
+	 */
+	public maxZoom(): number 
+	{
+		return Math.min(this.provider.maxZoom, this.heightProvider?.maxZoom ?? Infinity);
+	}
+
+	/**
+	 * Get map meta data from server if supported.
+	 */
+	public getMetaData(): void
+	{
+		this.provider.getMetaData();
+	}
+
+	public raycast(raycaster: Raycaster, intersects: any[]): boolean
+	{
+		return false;
+	}
+}
---
+++ vendor/geo-three/source/utils/UnitsUtils.ts
@@ -0,0 +1,203 @@
+import {Color, Vector2, Vector3} from 'three';
+import {Geolocation} from './Geolocation';
+
+/**
+ * Units utils contains methods to convert data between representations.
+ *
+ * Multiple methods are used to reprent world coordinates based on the type of data being presented.
+ * 
+ * WGS84 is the most commonly used representation with (latitude, longitude, altitude).
+ * 
+ * EPSG:900913 is used for planar coordinates in (X, Y, Z)
+ */
+export class UnitsUtils 
+{
+	/**
+	 * Average radius of earth in meters.
+	 */
+	public static EARTH_RADIUS: number = 6371008;
+
+	/**
+	 * Earth radius in semi-major axis A as defined in WGS84.
+	 */
+	public static EARTH_RADIUS_A: number = 6378137.0;
+
+	/**
+	 * Earth radius in semi-minor axis B as defined in WGS84.
+	 */
+	public static EARTH_RADIUS_B: number = 6356752.314245;
+
+	/**
+	 * Earth equator perimeter in meters.
+	 */
+	public static EARTH_PERIMETER: number = 2 * Math.PI * UnitsUtils.EARTH_RADIUS;
+
+	/**
+	 * Earth equator perimeter in meters.
+	 */
+	public static EARTH_ORIGIN: number = UnitsUtils.EARTH_PERIMETER / 2.0;
+
+	/**
+	 * Largest web mercator coordinate value, both X and Y range from negative extent to positive extent
+	 */
+	public static WEB_MERCATOR_MAX_EXTENT: number = 20037508.34;
+
+	/**
+	 * Converts coordinates from WGS84 Datum to XY in Spherical Web Mercator EPSG:900913.
+	 *
+	 * @param latitude - Latitude value in degrees.
+	 * @param longitude - Longitude value in degrees.
+	 */
+	public static datumsToSpherical(latitude: number, longitude: number): Vector2
+	{
+		const x = longitude * UnitsUtils.EARTH_ORIGIN / 180.0;
+		let y = Math.log(Math.tan((90 + latitude) * Math.PI / 360.0)) / (Math.PI / 180.0);
+
+		y = y * UnitsUtils.EARTH_ORIGIN / 180.0;
+
+		return new Vector2(x, y);
+	}
+
+	/**
+	 * Converts XY point from Spherical Web Mercator EPSG:900913 to WGS84 Datum.
+	 *
+	 * @param x - X coordinate.
+	 * @param y - Y coordinate.
+	 */
+	public static sphericalToDatums(x: number, y: number): Geolocation
+	{
+		const longitude = x / UnitsUtils.EARTH_ORIGIN * 180.0;
+		let latitude = y / UnitsUtils.EARTH_ORIGIN * 180.0;
+
+		latitude = 180.0 / Math.PI * (2 * Math.atan(Math.exp(latitude * Math.PI / 180.0)) - Math.PI / 2.0);
+
+		return new Geolocation(latitude, longitude);
+	}
+
+	/**
+	 * Converts quad tree zoom/x/y to lat/lon in WGS84 Datum.
+	 * 
+	 * The X and Y start from 0 from the top/left corner of the quadtree up to (4^zoom - 1)
+	 *
+	 * @param zoom - Zoom level of the quad tree.
+	 * @param x - X coordinate.
+	 * @param y - Y coordinate.
+	 */
+	public static quadtreeToDatums(zoom: number, x: number, y: number): Geolocation
+	{
+		const n = Math.pow(2.0, zoom);
+		const longitude = x / n * 360.0 - 180.0;
+		const latitudeRad = Math.atan(Math.sinh(Math.PI * (1.0 - 2.0 * y / n)));
+		const latitude = 180.0 * (latitudeRad / Math.PI);
+
+		return new Geolocation(latitude, longitude);
+	}
+
+	/**
+	 * Direction vector to WGS84 coordinates.
+	 * 
+	 * Can be used to transform surface points of world sphere to coordinates.
+	 * 
+	 * @param dir - Direction vector.
+	 * @returns WGS84 coordinates.
+	 */
+	public static vectorToDatums(dir: Vector3): Geolocation 
+	{
+		const radToDeg = 180 / Math.PI;
+
+		const latitude = Math.atan2(dir.y, Math.sqrt(Math.pow(dir.x, 2) + Math.pow(-dir.z, 2))) * radToDeg;
+		const longitude = Math.atan2(-dir.z, dir.x) * radToDeg;
+
+		return new Geolocation(latitude, longitude);
+	}
+
+	
+	/**
+	 * Get a direction vector from WGS84 coordinates.
+	 * 
+	 * The vector obtained will be normalized.
+	 * 
+	 * @param latitude - Latitude value in degrees.
+	 * @param longitude - Longitude value in degrees.
+	 * @returns Direction vector normalized.
+	 */
+	public static datumsToVector(latitude: number, longitude: number): Vector3 
+	{
+		const degToRad = Math.PI / 180;
+		
+		const rotX = longitude * degToRad;
+		const rotY = latitude * degToRad;
+
+		var cos = Math.cos(rotY);
+		
+		return new Vector3(-Math.cos(rotX + Math.PI) * cos, Math.sin(rotY), Math.sin(rotX + Math.PI) * cos);
+	}
+
+	/**
+	 * Get altitude from RGB color for mapbox altitude encoding
+	 * 
+	 * https://docs.mapbox.com/data/tilesets/guides/access-elevation-data/~
+	 * 
+	 * @param color - Color of the pixel
+	 * @returns The altitude encoded in meters.
+	 */
+	public static mapboxAltitude(color: Color): number 
+	{
+		return (color.r * 255.0 * 65536.0 + color.g * 255.0 * 256.0 + color.b * 255.0) * 0.1 - 10000.0;
+	}
+
+	/**
+	 * Get the size of a tile in web mercator coordinates
+	 * 	 * 
+	 * @param zoom - the zoom level of the tile
+	 * @returns the size of the tile in web mercator coordinates
+	 */
+	public static getTileSize(zoom: number): number
+	{
+		const maxExtent = UnitsUtils.WEB_MERCATOR_MAX_EXTENT;
+		const numTiles = Math.pow(2, zoom);
+		return 2 * maxExtent / numTiles;	
+	}
+
+	/**
+	 * Get the bounds of a tile in web mercator coordinates
+	 * 	 * 
+	 * @param zoom - the zoom level of the tile
+	 * @param x - the x coordinate of the tile
+	 * @param y - the y coordinate of the tile
+	 * @returns list of bounds - [startX, sizeX, startY, sizeY]
+	 */
+	public static tileBounds(zoom: number, x: number, y: number): number[]
+	{
+		const tileSize = UnitsUtils.getTileSize(zoom);
+		const minX = -UnitsUtils.WEB_MERCATOR_MAX_EXTENT + x * tileSize;
+		const minY = UnitsUtils.WEB_MERCATOR_MAX_EXTENT - (y + 1) * tileSize;
+		return [minX, tileSize, minY, tileSize];
+	}
+
+	/**
+	 * Get the latitude value of a given web mercator coordinate and zoom level
+	 * 
+	 * @param zoom - the zoom level of the coordinate
+	 * @param y - the y web mercator coordinate
+	 * @returns - latitude of coordinate in radians
+	 */
+	public static webMercatorToLatitude(zoom: number, y: number): number 
+	{
+		const yMerc = UnitsUtils.WEB_MERCATOR_MAX_EXTENT - y * UnitsUtils.getTileSize(zoom);
+		return Math.atan(Math.sinh(yMerc / UnitsUtils.EARTH_RADIUS));
+	}
+
+	/**
+	 * Get the latitude value of a given web mercator coordinate and zoom level
+	 * 
+	 * @param zoom - the zoom level of the coordinate
+	 * @param x - the x web mercator coordinate
+	 * @returns - longitude of coordinate in radians
+	 */
+	public static webMercatorToLongitude(zoom: number, x: number): number 
+	{
+		const xMerc = -UnitsUtils.WEB_MERCATOR_MAX_EXTENT + x * UnitsUtils.getTileSize(zoom);
+		return xMerc / UnitsUtils.EARTH_RADIUS;
+	}
+}
